917fae096cc805674270f2b50100cee4a49591a2
Ga-Alves <gabrielalvesreis@gmail.com>
2025-01-31 21:17:34 -0300

    test: :test_tube: trying valid test

--- fuzz_create_string.c
+++ fuzz_create_string.c
@@ -4,10 +4,5 @@
 
 int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size)
 {
-    cJSON *json_string = cJSON_CreateString((char *)data);
-    if (json_string)
-    {
-        cJSON_Delete(json_string);
-    }
     return 0;
 }
